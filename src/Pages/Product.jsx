16fbe82b385024d6418aa359efdf27bb48fddd90
ShivamP2E <144093375+ShivamP2E@users.noreply.github.com>
2023-10-23 22:06:21 +0530

    23oct commit

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -1,13 +1,19 @@
 import React, { useContext } from 'react'
 import { ShopContext } from '../Context/ShopContext'
+import { useParams } from 'react-router'
+import Breadcrums from '../components/Breadcrums/Breadcrums';
+import ProductDisplay from '../components/Productdisplay/ProductDisplay';
 // Product page: opens when we click on any image.
 const Product = () => {
-  const {all_products} = useContext(ShopContext)
+  const {all_product} = useContext(ShopContext);
+  const {productId} = useParams();
+  const product = all_product.find((e) => e.id === Number(productId))
   return (
     <div>
-      
+      <Breadcrums product = {product}/>
+      <ProductDisplay product= {product}/>
     </div>
   )
 }
 
-export default Product
+export default Product;
