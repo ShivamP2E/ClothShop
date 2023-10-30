ff1c5c02b1b2c591fe5d09ca3b9c087eb3e897d5
ShivamP2E <144093375+ShivamP2E@users.noreply.github.com>
2023-10-30 17:46:21 +0530

    30th 2nd commit

--- src/Pages/LoginSignup.jsx
+++ src/Pages/LoginSignup.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import '../Pages/Style/LoginSignup.css'
 import {signInWithGooglePopup} from '../firebase.util'
+import { createUserDocumentFromAuth } from '../firebase.util'
+
+
+
 const LoginSignup = () => {
   const logGoogleUser = async() => {
-    const response = await signInWithGooglePopup();
-    console.log(response);
+    const {user} = await signInWithGooglePopup();
+    const userDocRef = await createUserDocumentFromAuth(user);
 };
   return (
     <div className='loginsignup'>
@@ -16,7 +20,7 @@ const LoginSignup = () => {
           <input type='password' placeholder='Enter your password'/>
         </div>
         <button >Continue</button>
-        <button onClick={logGoogleUser}>Sign-up with Google</button>
+        <button className='bt-2' onClick={logGoogleUser}><img style={{width:'30px'}} src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/120px-Google_%22G%22_Logo.svg.png?20230822192911" alt="" srcset="" /></button>
         <p className='loginsignup-login'> Already have account? <span>Login here</span></p>
         <div className='loginsignup-agree'>
           <input type='checkbox' name='' id=''/>
--- src/firebase.util.js
+++ src/firebase.util.js
@@ -6,6 +6,13 @@ import {
     GoogleAuthProvider,
 } from 'firebase/auth'
 
+import {
+    getFirestore,
+    doc,
+    getDoc,
+    setDoc
+} from 'firebase/firestore'
+
 const firebaseConfig = {
     apiKey: "AIzaSyCJvBbVnqH-Jw7kYLjdeU95jHS3sniqrgE",
     authDomain: "clothshop-efbf2.firebaseapp.com",
@@ -24,3 +31,28 @@ const firebaseConfig = {
 
   export const auth = getAuth();
   export const signInWithGooglePopup = ()=> signInWithPopup(auth, provider);
+  export const db = getFirestore();
+  export const createUserDocumentFromAuth = async (userAuth) => {
+    const userDocRef = doc(db, 'users', userAuth.uid);
+    console.log(userDocRef);
+
+    const userSnapshot =await getDoc(userDocRef);
+    console.log(userSnapshot);
+    console.log(userSnapshot.exists());
+
+    if(!userSnapshot.exists()){
+        const {displayName, email}= userAuth;
+        const createdAt = new Date();
+        try{
+            await setDoc(userDocRef, {
+                displayName,
+                email,
+                createdAt,
+            });
+        }
+    catch(error){
+        console.log('error creating the user', error.message);
+    }
+    }
+    return userDocRef;
+  }
